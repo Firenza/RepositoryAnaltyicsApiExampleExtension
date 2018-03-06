f71a0f3b400bfd4a32726e9d44ffbe8834699e9c
Firenza <chadoftedahl@gmail.com>
2018-03-06 16:46:29 -0600

    Intiail commit

---
+++ src/RepositoryAnaltyicsApiExampleExtension/SimpleDevOpsIntegrationExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Composition;
+using System.Threading.Tasks;
+using RepositoryAnalyticsApi.Extensibility;
+using RepositoryAnalyticsApi.ServiceModel;
+
+namespace RepositoryAnaltyicsApiExampleExtension
+{
+    [Export(typeof(IDeriveRepositoryDevOpsIntegrations))]
+    public class SimpleDevOpsIntegrationExtension : IDeriveRepositoryDevOpsIntegrations
+    {
+        public Task<RepositoryDevOpsIntegrations> DeriveIntegrationsAsync(string repositoryName)
+        {
+            Console.WriteLine("Hey I'm a simple DevOps integraiton extension!");
+
+            return Task.FromResult<RepositoryDevOpsIntegrations>(null);
+        }
+    }
+}
---
+++ src/RepositoryAnaltyicsApiExampleExtension/SimpleTypeAndImplementationExtension.cs
@@ -0,0 +1,20 @@
+﻿using RepositoryAnalyticsApi.Extensibility;
+using RepositoryAnalyticsApi.ServiceModel;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Threading.Tasks;
+
+namespace RepositoryAnaltyicsApiExampleExtension
+{
+    [Export(typeof(IDeriveRepositoryTypeAndImplementations))]
+    public class SimpleTypeAndImplementationExtension : IDeriveRepositoryTypeAndImplementations
+    {
+        public Task<RepositoryTypeAndImplementations> DeriveImplementationAsync(IEnumerable<RepositoryDependency> dependencies, Func<Task<List<RepositoryFile>>> readFilesAsync, IEnumerable<string> topics, string name, Func<string, Task<string>> readFileContentAsync)
+        {
+            Console.WriteLine("Hey I'm a simple Type and Implementation extension!");
+
+            return Task.FromResult<RepositoryTypeAndImplementations>(null);
+        }
+    }
+}
